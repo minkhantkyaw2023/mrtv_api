f2e4c9a862bd1b7ca3471c475adb097da46edf6d
Nyi Ye Naing <nyiyenaing@securelinkmm.com>
2023-06-13 13:33:49 +0630

    .api routes changes in .htaccess

--- config/initialize.php
+++ config/initialize.php
@@ -1,6 +1,6 @@
 <?php 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT', "D:". DS .'LocalServer'.DS. 'Apache24'.DS. 'htdocs'.DS.'mrtvapi');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', "C:". DS . 'Apache24'.DS. 'htdocs'.DS.'mrtv_api');
 defined('CONFIG_PATH') ? null : define('CONFIG_PATH',SITE_ROOT.DS.'config');
 defined('OBJECTS_PATH') ? null : define('OBJECTS_PATH',SITE_ROOT.DS.'objects');
 defined('URL')? null : define('URL',"https://mrtv.gov.mm/");
